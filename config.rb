5ac3b1bb3a5250b5a111e864e0d52d2009f0cf48
Thomas Reynolds <me@tdreyno.com>
2012-04-24 16:48:08 -0700

    anonymize

--- config.rb
+++ config.rb
@@ -20,8 +20,8 @@ configure :build do
   activate :cache_buster
 
   # For example, change the Compass output style for deployment
-  activate :minify_css
+  # activate :minify_css
   
   # Minify Javascript on build
-  activate :minify_javascript
+  # activate :minify_javascript
 end
